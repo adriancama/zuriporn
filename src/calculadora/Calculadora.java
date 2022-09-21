fb3468b8bb2513889335cf2ced55493e49d50b85
adrian camarena heredia <adriancamarena2310@gmail.com>
2022-09-21 18:00:14 -0500

    first commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author HP}
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Ventana vent=new Ventana();
+       vent.setVisible(true);
+       vent.setSize(600,600);
+    
+    }
+    
+}
